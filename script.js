4ce336cf4cf418c2568dd7275ea15b21aed4dcc7
Allwin <87985223+AlwinAlphabot@users.noreply.github.com>
2022-09-03 12:59:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+var cursor = true;
+var speed = 450;
+
+setInterval(() => {
+   if(cursor) {
+     document.getElementById('cursor').style.opacity = 0;
+     cursor = false;
+   }else {
+     document.getElementById('cursor').style.opacity = 1;
+     cursor = true;
+   }
+}, speed);
